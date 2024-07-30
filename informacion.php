427d513ccabb67036db96182ae5200947ae3d78c
julyandrey <julianandrey0306@gmail.com>
2024-07-30 16:33:06 -0500

    Update informacion.php

--- informacion.php
+++ informacion.php
@@ -1,10 +1,10 @@
 <?Php
 $identificacion= $_POST ['identificacion'];
 $nomapellidos=$_POST ['nomapellidos'];
-$cargo=$_POST ['salario'];
-%salario=$_POST ['salario'];
+$cargo=$_POST ['cargo'];
+$salario=$_POST ['salario'];
 echo "la identificacion del empleado es: ".identicacion;
-echo "los nombre y apellidos del empleado son: ".nomapellidos
+echo "los nombre y apellidos del empleado son: ".nomapellidos;
 echo "el cargo del empleado es: ".cargo;
 echo "el salario del empleado es: ".salario;
 ?>
